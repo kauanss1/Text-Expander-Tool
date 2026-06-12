1f9909c287ea3726dd29a39b665117935fd026c0
kauanss1 <kauansantossilva138@gmail.com>
2026-06-12 20:43:36 -0300

    feat: criação automática de pastas e JSON na raiz do usuário

--- gestor_de_arquivos.py
+++ gestor_de_arquivos.py
@@ -4,24 +4,46 @@ import os
 
 class Gestor_de_arquivos:
     def __init__(self):
-        self.caminho_json = "gatilhos.Json"
+        
+
+        pasta_user = os.path.expanduser("~")
+        self.pasta_sistema = os.path.join(pasta_user,"Text-Expander-Tool")
+        self.caminho_json = os.path.join(self.pasta_sistema, "gatilhos.Json")
+        print(f" Tentando usar o caminho: {self.pasta_sistema}")
+
+        if not os.path.exists(self.pasta_sistema):
+            os.makedirs(self.pasta_sistema)
+            print("pasta criada ")
 
 
     def carregar_gatilhos(self):
 
         if not os.path.exists(self.caminho_json):
-            print("arquivos nao encontrado ")
-            return{}
-        
-        try:
-            with open (self.caminho_json, "r", encoding="utf-8") as arquivo:
-                dados = json.load(arquivo)
-                print(f"[Gestor] Sucesso! {len(dados)} gatilhos carregados do JSON.")
-            return dados
-        
+            print("arquivos nao encontrado criando nova pasta  ")
 
-        except Exception as e:
+            dados_iniciais = {
+                "\\help": "Olá! Sou o suporte técnico. Como posso te ajudar hoje?"
+            }
             
-            print(f"erro ao ler arquivos: {e}")
+        
+            try:
+                with open (self.caminho_json, "w", encoding="utf-8") as arquivo:
+                    json.dump(dados_iniciais, arquivo , ensure_ascii= False, indent=4)
+                    
+                    print(f" Sucesso! {len(dados_iniciais)} gatilhos carregados do JSON.")
+                return dados_iniciais
 
-            return{}
+            except Exception as e:
+                
+                print(f"erro ao ler arquivos: {e}")
+
+                return{}
+        try: 
+            with open(self.caminho_json, "r", encoding="utf-8") as arquivo:
+                dados = json.load(arquivo)
+                return dados
+       
+        except Exception as e:
+            print(f"dsadw {e}")
+      
+      
