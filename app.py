f520171478544b01a8d06768cb962a7a6a2beeb6
Kauan Santos Silva <kauansantossilva138@gmail.com>
2026-06-16 16:44:13 -0300

    Feat: estrutura basica da interface integrada com a bandeja via asyncio

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from teclado import teclado
 from criador_pastas import Obj_criador_de_pasta
 from bandeja_sistema import bandeja
 from threading import Thread
+import asyncio
 
 
 app_bandeja = None
@@ -13,7 +14,7 @@ def fechar(icon, item):
     print("fechando")
     if app_bandeja:
         app_bandeja.parar_icone()
-    os._exit
+    os._exit(0)
         
 
 def start_teclado():
@@ -22,16 +23,37 @@ def start_teclado():
     escutarr = teclado()
     ouvinte = keyboard.Listener(on_press=escutarr.tecla_ler)  
     ouvinte.start()
-        
+
+
+
+async def rodar_bandeja():
+    global app_bandeja
+    app_bandeja = bandeja(encerrar=fechar)
+    await asyncio.get_event_loop().run_in_executor(None, app_bandeja.criar_icon)
 
 
 if __name__ == '__main__':
 
     segundo_plano = Thread(target=start_teclado, daemon=True)
     segundo_plano.start()
+    interface = webview.create_window(
+        title="Text Expander",
+        url = "interface/index.html",
+        width=700,
+        height=500,
+        resizable=True
+        )
+    webview.start()
+
+    # thread_interface = Thread(target=start_interface, daemon=True)
+    # thread_interface.start()
 
 
     app_bandeja = bandeja(encerrar=fechar)
     app_bandeja.criar_icon()
+
+    def iniciar_loop():
+        asyncio.run(rodar_bandeja())
+    webview.start(iniciar_loop)
 
         
