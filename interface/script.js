66b5773db169859a71d2c79caa5b9c6701e73606
Kauan Santos Silva <kauansantossilva138@gmail.com>
2026-07-01 17:23:48 -0300

    Feat: alternancia de telas main e novo-atalho usando getElementById

---
+++ interface/script.js
@@ -0,0 +1,12 @@
+
+function aoClicarBotao() {
+    
+    const menuPrincipal = document.getElementById('pagina_inicial');
+    const telaCadastro = document.getElementById('cadastro_atalhos');
+
+
+    menuPrincipal.style.display = 'none';
+
+    
+    telaCadastro.style.display = 'block'; 
+}
