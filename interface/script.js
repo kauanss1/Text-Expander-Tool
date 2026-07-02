76b6db70408cca82498559168a8ff41be8e9c66d
Kauan Santos Silva <kauansantossilva138@gmail.com>
2026-07-02 17:14:50 -0300

    Design: melhorias na estilizacao e refinamento da parte visual

--- interface/script.js
+++ interface/script.js
@@ -1,20 +1,9 @@
-
 function btn_abri() {
-    
-    const menuPrincipal = document.getElementById('pagina_inicial');
     const telaCadastro = document.getElementById('cadastro_atalhos');
-
-
-    menuPrincipal.style.display = 'none';
-
-    
-    telaCadastro.style.display = 'flex'; 
+    telaCadastro.style.display = 'block'; 
 }
 
 function btn_fechar() {
-    const menuPrincipal = document.getElementById('pagina_inicial');
     const telaCadastro = document.getElementById('cadastro_atalhos');
-
-    menuPrincipal.style.display = 'flex'; 
     telaCadastro.style.display = 'none'; 
 }
